ece4b330096189ed4b7417fb26fbf90d578f60ef
psikrishna <attalurisaikrishna@gmail.com>
2020-06-27 16:04:56 +0530

    deployment 3

---
+++ apiKeys.js
@@ -0,0 +1,9 @@
+// store api keys for all node projects of this course here
+module.exports = {
+  'weather_api_key': '7c712e661230924a55f99087b7702958',
+  'mapbox_api_key': 'pk.eyJ1IjoicHNpa3Jpc2huYSIsImEiOiJja2JwZWM4NGkxNHkwMnltaThlc2F3d3RsIn0.J5crHQFpzQmgL1alHAJMEQ',
+};
+
+// changes counter
+// remember to increment only after committing
+// counter value : 24
